4ae47af35af0ce8c1d84d7ee2d26d5c173bbc213
Justin Karimi <jekhokie@gmail.com>
2013-06-21 12:46:42 -0500

    Add Spec for Instance application_list Method
    Update the Instance spec to include tests for the application_list
    method.

--- spec/tomcat_manager/instance_spec.rb
+++ spec/tomcat_manager/instance_spec.rb
@@ -134,4 +134,57 @@ describe Tomcat::Manager::Instance do
       end
     end
   end
+
+  describe "application_list" do
+    let(:host)             { "my.host"    }
+    let(:port)             { "9001"       }
+    let(:manager_username) { "admin"      }
+    let(:manager_password) { "s3cre7Pas$" }
+    let(:manager_instance) { FactoryGirl.build :instance, :host => host,
+                                                          :port => port,
+                                                          :manager_username => manager_username,
+                                                          :manager_password => manager_password }
+
+    describe "for an instance with applications deployed" do
+      before :each do
+        FakeWeb.register_uri(:get, "http://#{manager_username}:#{manager_password}@#{host}:#{port}/manager/text/list",
+                                   :body => File.open(File.dirname(__FILE__) + "/../fixtures/application_list.txt", "r").read,
+                                   :status => [ "200", "OK" ])
+      end
+
+      it "returns a hash of deployed applications" do
+        manager_instance.application_list.should ==
+          { "/"                     => { :status => "running", :sessions => 0, :name => "ROOT"                 },
+            "/manager"              => { :status => "running", :sessions => 2, :name => "manager"              },
+            "/docs"                 => { :status => "running", :sessions => 0, :name => "docs"                 },
+            "/test-app-1.0.0"       => { :status => "running", :sessions => 2, :name => "test-app-1.0.0"       },
+            "/test-app-2.0.1"       => { :status => "running", :sessions => 0, :name => "test-app-2.0.1"       },
+            "/other-test-app-1.0.1" => { :status => "stopped", :sessions => 0, :name => "other-test-app-1.0.1" },
+            "/examples"             => { :status => "running", :sessions => 1, :name => "examples"             },
+            "/host-manager"         => { :status => "running", :sessions => 0, :name => "host-manager"         } }
+      end
+    end
+
+    describe "for a non-reachable server" do
+      let(:manager_instance) { FactoryGirl.build :instance }
+
+      it "raises an exception" do
+        expect{ manager_instance.application_list }.to raise_error
+      end
+    end
+
+    describe "for invalid connection credentials" do
+      let(:manager_instance) { FactoryGirl.build :instance, :host => host, :port => port }
+
+      before :each do
+        FakeWeb.register_uri(:get, "http://#{host}:#{port}/manager/text/list",
+                                   :body => File.open(File.dirname(__FILE__) + "/../fixtures/invalid_connect.html", "r").read,
+                                   :status => [ "401", "Unauthorized" ])
+      end
+
+      it "raises an exception" do
+        expect{ manager_instance.application_list }.to raise_error
+      end
+    end
+  end
 end
