02d70c361e56ce3b03b3a461b33e9bc0c6544c1e
Justin Karimi <jekhokie@gmail.com>
2013-06-20 19:53:04 -0500

    Add Tomcat::Manager new Method
    Update the Tomcat::Manager module to include a new method.

--- lib/tomcat-manager.rb
+++ lib/tomcat-manager.rb
@@ -1,7 +1,29 @@
-require "tomcat-manager/version"
+require_relative "tomcat-manager/version"
+require_relative "tomcat-manager/instance"
 
 module Tomcat
   module Manager
-    # Your code goes here...
+    class << self
+      # Public: Initializes a new TomcatManager::Instance.
+      #
+      # host           - Host on which the Tomcat server is running.
+      # port           - Optional port on which the Tomcat Manager is listening (default = 8080).
+      # admin_user     - Optional username for the administrative user for the Tomcat Manager application (default = admin).
+      # admin_password - Optional password for the administrative user for the Tomcat Manager application (default = admin).
+      # options        - Optional hash used to configure this TomcatManager (default = nil).
+      #                    :api_version - Version of the TomcatManager API to use (drives the schema)
+      #
+      # Examples
+      #
+      #   Tomcat::Manager.new :host           => 'http://myHost.internal.com',
+      #                       :port           => 9090,
+      #                       :admin_password => 'my$ecr3tP4SS'
+      #
+      # Returns a TomcatManager::Instance.
+      #
+      def new(args = nil)
+        Tomcat::Manager::Instance.new args
+      end
+    end
   end
 end
