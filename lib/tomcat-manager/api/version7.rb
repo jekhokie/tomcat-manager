a587340369398b62dc90c595327b52189c26aabe
Justin Karimi <jekhokie@gmail.com>
2013-06-21 12:26:17 -0500

    Update Version7 for deployed_applications
    Update the Version7 class to include a deployed_applications method that
    returns a hash of currently deployed applications along with meta
    information.

--- lib/tomcat-manager/api/version7.rb
+++ lib/tomcat-manager/api/version7.rb
@@ -17,6 +17,21 @@ module Tomcat
         def connect_response_valid?(response_code)
           response_code == "200"
         end
+
+        def deployed_applications(response)
+          application_list = Hash.new
+
+          response.split("\n")[1..-1].each do |app_info|
+            app_components = app_info.split(':')
+            (application_list[(app_components[3] == "ROOT" ? "/" : app_components[0])] = Hash.new).tap do |application_hash|
+              application_hash[:status]   = app_components[1]
+              application_hash[:sessions] = app_components[2].to_i
+              application_hash[:name]     = app_components[3]
+            end
+          end
+
+          application_list
+        end
       end
     end
   end
