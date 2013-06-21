1dfbc4c545204eb02792cd04bcbf4cbc532f92cd
Justin Karimi <jekhokie@gmail.com>
2013-06-20 21:03:23 -0500

    Add Version7 Class
    Add a Version7 class.

--- lib/tomcat-manager/api/version7.rb
+++ lib/tomcat-manager/api/version7.rb
@@ -2,6 +2,13 @@ module Tomcat
   module Manager
     module Api
       class Version7
+        ###########################
+        # URL Response Methods
+        ###########################
+
+        def connect_path
+          "/manager/text/list"
+        end
       end
     end
   end
