fb9eca2d0fa229fe3b438df6d3c385e5d80343f8
Justin Karimi <jekhokie@gmail.com>
2013-06-20 20:36:19 -0500

    Update Api Spec for supported_versions Method
    Add the supported_verisons method to the Api spec.

--- spec/tomcat_manager/api_spec.rb
+++ spec/tomcat_manager/api_spec.rb
@@ -6,4 +6,8 @@ describe Tomcat::Manager::Api do
   it "can create a new instance" do
     subject.should respond_to(:new)
   end
+
+  it "returns an array of supported versions" do
+    Tomcat::Manager::Api.supported_versions.should be_instance_of(Array)
+  end
 end
