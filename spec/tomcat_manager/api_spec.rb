70e3cae7e0da6c68a55565a4c7157cf4903b5c5f
Justin Karimi <jekhokie@gmail.com>
2013-06-20 20:41:05 -0500

    Update Api Spec to Include new Method
    Update the Api spec to include testing the new method.

--- spec/tomcat_manager/api_spec.rb
+++ spec/tomcat_manager/api_spec.rb
@@ -10,4 +10,14 @@ describe Tomcat::Manager::Api do
   it "returns an array of supported versions" do
     Tomcat::Manager::Api.supported_versions.should be_instance_of(Array)
   end
+
+  describe "new" do
+    it "returns a new Api instance" do
+      Tomcat::Manager::Api.new("7").should be_instance_of(Tomcat::Manager::Api::Version7)
+    end
+
+    it "raises an error for an unknown version" do
+      expect{ Tomcat::Manager::Api.new("X") }.to raise_error
+    end
+  end
 end
