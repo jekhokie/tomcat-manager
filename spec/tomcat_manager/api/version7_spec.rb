c7693c3f0c8252a9014fa8af2bbe142e26740911
Justin Karimi <jekhokie@gmail.com>
2013-06-21 12:25:43 -0500

    Add Spec for Version7 deployed_applications Method
    Add a spec for the Version7 class deployed_applications method.

--- spec/tomcat_manager/api/version7_spec.rb
+++ spec/tomcat_manager/api/version7_spec.rb
@@ -21,5 +21,22 @@ describe Tomcat::Manager::Api::Version7 do
         api.connect_response_valid?("abc").should == false
       end
     end
+
+    describe "deployed_applications" do
+      let(:api)              { FactoryGirl.build :api, :version => "7" }
+      let(:application_list) { File.open(File.dirname(__FILE__) + "/../../fixtures/application_list.txt", "r").read }
+
+      it "returns a hash of applications" do
+        api.deployed_applications(application_list).should ==
+          { "/"                     => { :status => "running", :sessions => 0, :name => "ROOT"                 },
+            "/manager"              => { :status => "running", :sessions => 2, :name => "manager"              },
+            "/docs"                 => { :status => "running", :sessions => 0, :name => "docs"                 },
+            "/test-app-1.0.0"       => { :status => "running", :sessions => 2, :name => "test-app-1.0.0"       },
+            "/test-app-2.0.1"       => { :status => "running", :sessions => 0, :name => "test-app-2.0.1"       },
+            "/other-test-app-1.0.1" => { :status => "stopped", :sessions => 0, :name => "other-test-app-1.0.1" },
+            "/examples"             => { :status => "running", :sessions => 1, :name => "examples"             },
+            "/host-manager"         => { :status => "running", :sessions => 0, :name => "host-manager"         } }
+      end
+    end
   end
 end
